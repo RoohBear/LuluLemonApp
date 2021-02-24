b65cb91f4526dea4b741014d012cf962f01d604d
Greg Wishart <greg.wishart@gmail.com>
2021-02-24 16:53:55 -0500

    Wrote a test to see that items are added to the array, and that invalid items are not added

--- UIKitVersionTests/UIKitVersionTests.swift
+++ UIKitVersionTests/UIKitVersionTests.swift
@@ -8,22 +8,42 @@
 import XCTest
 @testable import UIKitVersion
 
-class UIKitVersionTests: XCTestCase {
-
-    override func setUpWithError() throws {
+class UIKitVersionTests: XCTestCase
+{
+	var model = LuluModel()
+	
+	override func setUpWithError() throws
+	{
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDownWithError() throws
+	{
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
+    func testExample() throws
+	{
+		DispatchQueue.main.async {
+			self.model.addGarment(name:"fooh")
+			XCTAssert(self.model.arrGarments.count == 1)
+
+			self.model.addGarment(name:"pooh")
+			XCTAssert(self.model.arrGarments.count == 2)
+
+			self.model.addGarment(name:"booh")
+			XCTAssert(self.model.arrGarments.count == 3)
+
+			self.model.addGarment(name:"")	// this should not get added so the count should still be 3
+			XCTAssert(self.model.arrGarments.count == 3)
+		}
+		
+		// This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() throws
+	{
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
