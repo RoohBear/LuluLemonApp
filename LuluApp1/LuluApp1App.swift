fcc13082cd9e03c60a34d4a763437600a64dc60c
Greg Wishart <greg.wishart@gmail.com>
2021-02-23 22:13:12 -0500

    Initial Commit

---
+++ LuluApp1/LuluApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  LuluApp1App.swift
+//  LuluApp1
+//
+//  Created by G Bear on 2021-02-23.
+//
+
+import SwiftUI
+
+@main
+struct LuluApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
