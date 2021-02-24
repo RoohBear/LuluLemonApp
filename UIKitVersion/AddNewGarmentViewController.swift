5fc3cab20c6e7117f9a8e5d436acf226ed620f69
Greg Wishart <greg.wishart@gmail.com>
2021-02-24 15:30:20 -0500

    Laying down foundation of UIKit version

---
+++ UIKitVersion/AddNewGarmentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddNewGarmentViewController.swift
+//  UIKitVersion
+//
+//  Created by G Bear on 2021-02-24.
+//
+
+import Foundation
+import UIKit
+
+class AddNewGarmentViewController : UIViewController
+{
+	
+	
+}
+
--- UIKitVersion/FirstViewController.swift
+++ UIKitVersion/FirstViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
-class FirstViewController: UIViewController
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+	var model = LuluModel()
+
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
 	
 	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return model.arrGarments.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		var ret = tableView.dequeueReusableCell(withIdentifier:"cell")
+		if ret == nil {
+			ret = UITableViewCell.init(style:.default, reuseIdentifier:"cell")
+		}
+		
+		return ret!
+	}
+	
+
+
+
 }
 
