4c2b4b7a0410a033323bcb46b0e3b6f347cfe9c2
Greg Wishart <greg.wishart@gmail.com>
2021-02-24 15:12:09 -0500

    Added the UI to add a new garment. Table now automatically gets sorted 5 seconds after startup.  Table gets sorted automatically after user adds anything

--- LuluApp1/ContentView.swift
+++ LuluApp1/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View
 {
 	@ObservedObject var model:LuluModel
 	var df = DateFormatter.init()
+	@State private var showAddGarmentView = false
+	@State private var newGarmentName = ""
 
 	var body: some View
 	{
@@ -24,9 +26,45 @@ struct ContentView: View
 					Spacer()
 					Button(action: {
 						print("Add button was tapped")
+						newGarmentName = ""
+						self.showAddGarmentView = true
 					}) {
 						Image(systemName: "plus.circle").font(.largeTitle)
-					}
+					}.popover(isPresented: $showAddGarmentView, content: {
+						// Title string and Save button at the top
+						ZStack {
+							HStack {
+								Text("Add")
+							}
+							HStack {
+								Spacer()
+								Button(action: {
+									print("Save button was tapped")
+									self.model.addGarment(name: newGarmentName)
+									self.showAddGarmentView = false
+								}) {
+									Text("Save")
+								}
+							}
+						}.padding().border(Color.black, width: 2)
+
+						// Label and text field go here
+						VStack {
+							HStack {
+								Text("Garment Name:")
+								Spacer()
+							}
+							HStack {
+								TextField("Enter garment name", text:$newGarmentName, onEditingChanged: { began in
+									print("Hello from onediting changed")
+								})
+								Spacer()
+							}
+						}
+						
+						// push everything up and leave space at the bottom
+						Spacer()
+					})
 				}.padding().border(Color.black, width: 2)
 			}
 
@@ -34,7 +72,7 @@ struct ContentView: View
 			HStack {
 				Button(action: {
 					print("Alpha button tapped")
-					model.doSort(method:.name)
+					model.doSort(how:.name)
 				}) {
 					Text("Alpha")
 				}.background(model.currentSortMethod == .name ? Color.black : Color.clear)
@@ -43,23 +81,26 @@ struct ContentView: View
 
 				Button(action: {
 					print("Creation time button tapped")
-					model.doSort(method:.creationDate)
+					model.doSort(how:.creationDate)
 				}) {
 					Text("Creation Time")
 				}.background(model.currentSortMethod == .creationDate ? Color.black : Color.clear)
 			}
 
+			// The table of garments (each row showing the garment name and creation date)
 			List(model.arrGarments, id: \.name) { garment in
 				HStack
 				{
 					Text(garment.name)
 					Spacer()
 					Text(garment.creationDateForUI())
-				}
+				}.frame(width:350, height:50)
 			}
+
+			// The spacer pushes everything up to leave space at the bottom
 			Spacer()
 		}.onAppear() {
-			// add 4 things to the queue 1 second apart so they have obvious different creation times.
+			// on ViewDidAppear, we add 4 things to the queue 1 second apart so they have obvious different creation times.
 			DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + TimeInterval(1)) {
 				model.addGarment(name: "T-Shirt")
 			}
@@ -71,6 +112,16 @@ struct ContentView: View
 			}
 			DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + TimeInterval(4)) {
 				model.addGarment(name: "Pant")
+			}
+			
+
+			// On a normal app, it would be good for the model to have a default sort method.
+			// In this app, the default is ".unknown" but just for fun, let's set the sort method
+			// automatically 5 seconds after startup to ".name" to demonstrate how the UI updates automatically.
+			DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + TimeInterval(5)) {
+				DispatchQueue.main.async {
+					model.doSort(how:.name)
+				}
 			}
 		}
 	}
--- LuluApp1/LuluModel.swift
+++ LuluApp1/LuluModel.swift
@@ -40,12 +40,12 @@ class LuluModel : ObservableObject
 		case creationDate
 	}
 
+	// make these read-only variables
 	@Published private(set) var arrGarments = [GarmentInfo]()
-	var currentSortMethod:SortMethod = .unsorted
-
+	@Published private(set) var currentSortMethod:SortMethod = .unsorted
 	
 	/**
-	 * Adds a garment to the array of garments, then performs a sort.
+	 * Adds a garment to the array of garments, then performs a sort using the current sort method (if any)
 	 * @param name Name of the garment.
 	 */
 	func addGarment(name:String)
@@ -53,17 +53,18 @@ class LuluModel : ObservableObject
 		DispatchQueue.main.async {
 			// this must be done on main thread because it publishes a change
 			self.arrGarments.append(GarmentInfo(name:name, creationDate:Date.init()))
+			self.doSort(how:self.currentSortMethod)
 		}
 	}
 	
 	/**
 	 * Sorts arrGaments in place based on the method chosen (by name, or creation date).
-	 * @param method Must be one of the SortMethod types
+	 * @param how Must be one of the SortMethod types
 	 */
-	func doSort(method: SortMethod)
+	func doSort(how: SortMethod)
 	{
 		arrGarments.sort { leftHandSide, rightHandSide in
-			switch method {
+			switch how {
 				case .name:
 					if leftHandSide.name < rightHandSide.name {
 						return true
@@ -81,7 +82,7 @@ class LuluModel : ObservableObject
 			}
 		}
 
-		self.currentSortMethod = method
+		self.currentSortMethod = how
 	}
 }
 
